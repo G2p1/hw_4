2ef7ce965c05a2ec99f2a6aa31d982088d6ae442
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-05-01 18:56:26 +0300

    Mak all solution

---
+++ HW_4/Controllers/StartFormController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW_4.Controllers
+{
+    public class StartFormController : Controller
+    {
+        // GET: StartForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Congrats(string name, string email)
+        {
+            
+            return HttpUtility.HtmlEncode("Я, "+name+"("+email+")"+"став розробником ASP .NET");
+            
+        }
+    }
+}
